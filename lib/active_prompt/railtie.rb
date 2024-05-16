835dbc524bf98274e662609e5168b3b52f752e53
Shane Perreault <shaneprrlt@gmail.com>
2024-05-15 23:43:11 -0600

    fix generator initialization

--- lib/active_prompt/railtie.rb
+++ lib/active_prompt/railtie.rb
@@ -2,8 +2,8 @@ require 'rails/railtie'
 
 module ActivePrompt
   class Railtie < Rails::Railtie
-    config.app_generators do |g|
-      g.templates.unshift File.expand_path('../generators/active_prompt', __dir__)
+    generators do
+      require_relative 'generators/active_prompt/prompt/prompt_generator'
     end
   end
 end
