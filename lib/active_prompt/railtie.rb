55455811b98c824ea5d5a8651daf35d5c3526ac9
Shane Perreault <shaneprrlt@gmail.com>
2024-05-16 01:20:27 -0600

    fixes

--- lib/active_prompt/railtie.rb
+++ lib/active_prompt/railtie.rb
@@ -3,6 +3,7 @@
 require "rails/railtie"
 
 module ActivePrompt
+  # This class is a Railtie that allows ActivePrompt to be loaded as a Rails engine
   class Railtie < Rails::Railtie
     config.eager_load_namespaces << ActivePrompt
   end
